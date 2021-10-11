78ebfc45c2cb0c971a6dd55b6ddba059f1ac9a75
Zentari <zenti.asteris@gmail.com>
2021-10-12 00:20:15 +0300

    Create main services

---
+++ chat-node-service/src/main/java/dev/zentari/chatnodeservice/ChatNodeServiceApplication.java
@@ -0,0 +1,13 @@
+package dev.zentari.chatnodeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatNodeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatNodeServiceApplication.class, args);
+    }
+
+}
---
+++ chat-node-service/src/test/java/dev/zentari/chatnodeservice/ChatNodeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.zentari.chatnodeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatNodeServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
